2665799503159a5e0394de3cb85690e55ce69962
MontagabAlHamawy <ma0152260@gmail.com>
2024-05-01 18:13:17 +0300

    first commit

--- src/components/Articles/articleItem.tsx
+++ src/components/Articles/articleItem.tsx
@@ -12,7 +12,7 @@ export default function ArticleItem({article}:ArticleItemProps) {
             <h1 className="text-lg font-bold text-gray-900 line-clamp-1">{article.title}</h1>
             <p className="text-base text-gray-500 my-2 p-1 line-clamp-1">{article.body}</p>
             <Link className="text-xl bg-purple-700 hover:bg-purple-900 w-full block text-center p-1 text-white rounded-lg" href={`/articles/${article.id}`}>
-                Read More
+                Read More ...
             </Link>
         </div>
     )
